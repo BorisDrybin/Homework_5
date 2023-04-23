16ffbc89ca46ee9cf8e5438f7a36ccd9038e89ea
Boris Drybin <makoh93@gmail.com>
2023-04-23 22:14:21 +0300

    second task added

--- Program.cs
+++ Program.cs
@@ -2,22 +2,48 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+// Console.WriteLine("Введите размер массива");
+
+// int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
+
+// int count = 0;
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     arr[i] = new Random().Next(100, 1000);
+
+//     if (arr[i] % 2 == 0)
+//     {
+//         count = count + 1;
+//     }
+// }
+
+// Console.WriteLine(String.Join(", ",arr) 
+// +"\nКоличество четных чисел в массиве равно: "+count);
+
+
+
+
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
 Console.WriteLine("Введите размер массива");
 
 int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
 
-int count = 0;
+int sum = 0;
 
 for (int i = 0; i < arr.Length; i++)
 {
     arr[i] = new Random().Next(100, 1000);
 
-    if (arr[i] % 2 == 0)
+    if (i % 2 != 0)
     {
-        count = count + 1;
+        sum = sum + arr[i];
     }
 }
 
 Console.WriteLine(String.Join(", ",arr) 
-+"\nКоличество четных чисел в массиве равно: "+count);
-
++"\nСумма элементов с нечетными идексами равна: "+sum);
