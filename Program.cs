e5df09b3dcead82fa40579beac00da0234be4506
Boris Drybin <makoh93@gmail.com>
2023-04-23 22:27:32 +0300

    third task corrected

--- Program.cs
+++ Program.cs
@@ -29,9 +29,40 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+// Console.WriteLine("Введите размер массива");
+
+// int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
+
+// int sum = 0;
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     arr[i] = new Random().Next(-100, 101);
+
+//     if (i % 2 != 0)
+//     {
+//         sum = sum + arr[i];
+//     }
+// }
+
+// Console.WriteLine(String.Join(", ",arr) 
+// +"\nСумма элементов с нечетными идексами равна: "+sum);
+
+
+
+
+
+// Задайте массив вещественных чисел. Найдите разницу 
+// между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
 Console.WriteLine("Введите размер массива");
 
 int[] arr = new int[Convert.ToInt32(Console.ReadLine())];
+
+int max = arr[0];
+int min = arr[0];
 
 int sum = 0;
 
@@ -39,11 +70,17 @@ for (int i = 0; i < arr.Length; i++)
 {
     arr[i] = new Random().Next(-100, 101);
 
-    if (i % 2 != 0)
+    if (arr[i] <= min)
     {
-        sum = sum + arr[i];
+        min = arr[i];
+    }
+    else if (arr[i] >= max)
+    {
+        max = arr[i];
     }
 }
 
+sum = max - min;
+
 Console.WriteLine(String.Join(", ",arr) 
-+"\nСумма элементов с нечетными идексами равна: "+sum);
++"\nРазница между максимальным и минимальным элементами равна: "+sum);
